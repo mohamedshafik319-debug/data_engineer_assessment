e4b51587bf1048196eb7232ea4f5d432b56b8bdb
SHAFI <mohamedshafik319@gmail.com>
2025-10-26 11:43:21 +0530

    Add SQL script to create table
    Created tables for customers and orders with relevant fields.

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,18 @@
+-- create_tables.sql
+-- Created by Mohamed Shafik for 100x Data Engineer Assessment
+
+create table customers (
+    customer_id int primary key,
+    first_name varchar(50),
+    last_name varchar(50),
+    email varchar(100),
+    signup_date date
+);
+
+create table orders (
+    order_id int primary key,
+    customer_id int,
+    order_date date,
+    amount decimal(10,2)
+);
 
